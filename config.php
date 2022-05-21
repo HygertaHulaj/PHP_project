c8b5020476c4860683cac7f47376adc0c57f8845
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2022-05-21 23:26:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,4 +5,5 @@ $db_pass = "";
 $db_name = "regjistrimi";
 
 $db = new PDO('mysql:host=localhost;db_name=' . db_name. ' charset=utf8', $db_user, $db_pass);
-$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?> 
