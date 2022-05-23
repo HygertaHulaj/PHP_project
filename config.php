023243b49fc49f317b7596473573a30f757abc12
dionberisha23 <94796934+dionberisha23@users.noreply.github.com>
2022-05-23 13:15:53 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_user = "projekti";
-$db_pass = "";
-$db_name = "regjistrimi";
-
-$db = new PDO('mysql:host=localhost;db_name=' . $db_name. ' charset=utf8', $db_user, $db_pass);
-$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
